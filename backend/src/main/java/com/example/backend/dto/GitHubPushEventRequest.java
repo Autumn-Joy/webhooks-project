275d31ccecc6a877030918f908dc2f2030cd60fc
Autumn Hicks <ajhicks1511@gmail.com>
2026-06-15 18:14:18 -0400

    add Jackson dependency and update fields

--- backend/src/main/java/com/example/backend/dto/GitHubPushEventRequest.java
+++ backend/src/main/java/com/example/backend/dto/GitHubPushEventRequest.java
@@ -1,15 +1,28 @@
 package com.example.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class GitHubPushEventRequest {
+
+    @JsonProperty("ref")
     private String ref;
+
+    @JsonProperty("before")
     private String before;
+
+    @JsonProperty("after")
     private String after;
+
+    @JsonProperty("git_url")
     private String git_url;
+
+    @JsonProperty("repository")
     private RepositoryDTO repository;
+
+    @JsonProperty("pusher")
     private PusherDTO pusher;
 }
